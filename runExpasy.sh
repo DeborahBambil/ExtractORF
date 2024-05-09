31c2ab5e042c9b339dfe615eea9d52af1f5bc9bd
DeborahBambil <50251383+DeborahBambil@users.noreply.github.com>
2024-05-09 15:32:38 -0300

    Create runExpasy.sh

---
+++ runExpasy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while IFS= read -r line
+do
+    if [[ $line =~ ^\>.+ ]]; then
+        title=$(echo "$line")
+        echo "$title" >> input.fasta
+    else
+        sequence=$(echo "$line" | sed 's/ACTG/'"$line"'/')
+        curl -s -d "dna_sequence=$sequence&output_format=fasta&style=raw" https://web.expasy.org/cgi-bin/translate/dna2aa.cgi >> my_output.fasta
+    fi
+done < sequencias.fa
