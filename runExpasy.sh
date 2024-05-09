9fc0e057d5ac85f818e646f9419945d2b058f47c
DeborahBambil <50251383+DeborahBambil@users.noreply.github.com>
2024-05-09 15:34:21 -0300

    Update runExpasy.sh

--- runExpasy.sh
+++ runExpasy.sh
@@ -7,6 +7,6 @@ do
         echo "$title" >> input.fasta
     else
         sequence=$(echo "$line" | sed 's/ACTG/'"$line"'/')
-        curl -s -d "dna_sequence=$sequence&output_format=fasta&style=raw" https://web.expasy.org/cgi-bin/translate/dna2aa.cgi >> my_output.fasta
+        curl -s -d "dna_sequence=$sequence&output_format=fasta&style=raw" https://web.expasy.org/cgi-bin/translate/dna2aa.cgi >> input.fasta
     fi
 done < sequencias.fa
